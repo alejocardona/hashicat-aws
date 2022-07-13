88a7ad2301e8a9888e9ad0ac84270a8d6fed633a
Alejandro Cardonae <cardalex@hotmail.comm>
2022-07-13 23:05:03 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/alejo-pruebas/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket = "terraf-my-bucketico"
   acl    = "private"
 
   versioning = {
